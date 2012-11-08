b17b5d5b8ccc8ca034f2e97b28a9c33e170bb972
Mark Newton <mnewton@redhat.com>
2012-11-08 18:11:49 +0100

    Added first draft of HAML layout templates and themes showcase pages.

---
+++ javascripts/subnavstick.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+    var $win = $(window)
+      , $nav = $('.subnav')
+      , navTop = $('.subnav').length && $('.subnav').offset().top - 54
+      , isFixed = 0
+
+    processScroll()
+
+    $win.on('scroll', processScroll)
+
+    function processScroll() {
+      var i, scrollTop = $win.scrollTop()
+      if (scrollTop >= navTop && !isFixed) {
+        isFixed = 1
+        $nav.addClass('subnav-fixed')
+      } else if (scrollTop <= navTop && isFixed) {
+        isFixed = 0
+        $nav.removeClass('subnav-fixed')
+      }
+    }
