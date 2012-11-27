f36e4f0eca779280ec0ac662cb2143fe49fe7d54
unibrew <ryszard.kozmik@gmail.com>
2012-11-27 20:11:06 +0100

    JavaScript, CSS and HTML minimizers and file merger awestruct extensions.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,5 +1,13 @@
+require 'js_minifier'
+require 'css_minifier'
+require 'html_minifier'
+require 'file_merger'
 
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::Partial
+  transformer Awestruct::Extensions::JsMinifier.new
+  transformer Awestruct::Extensions::CssMinifier.new
+  transformer Awestruct::Extensions::HtmlMinifier.new
+  extension Awestruct::Extensions::FileMerger.new
 end
 
