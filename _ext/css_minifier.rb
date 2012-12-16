9d660480d4f7eef243aa028607dc2a090b995a2f
unibrew <ryszard.kozmik@gmail.com>
2012-12-16 12:44:41 +0100

    Improved JavaScript and CSS minifiers to let user define a suffix added to each minified file name.

--- _ext/css_minifier.rb
+++ _ext/css_minifier.rb
@@ -21,10 +21,12 @@ require 'cssminify'
 #
 # 2. In your site.yml add:
 #
-#    css_minifier: enabled
+#    css_minifier:
+#      enabled: true
+#      fileSuffix: min
 #
-#    This setting is optional and defaults to 'enabled', it's useful when using different configurations
-#    for different runtime profiles.
+#    This setting is optional. 'enabled' defaults to 'true' and 'fileSuffix' defaults to an empty string.
+#    Which implies minification of file content in place. If 'fileSuffix' is given then a new file is created.
 #
 ##
 module Awestruct
@@ -35,21 +37,61 @@ module Awestruct
 
         # Checking if 'css_minifier' setting is provided and whether it's enabled.
         # By default, if it's not provided, we imply it's enabled.
-        if !site.css_minifier.nil? && !site.css_minifier.to_s.eql?("enabled")
+        if !site.css_minifier.nil? and !site.css_minifier['enabled'].nil? and site.css_minifier['enabled'].to_s.eql?("false")
           return input
         end
+
+        fileSuffix = nil
+        if !site.css_minifier.nil? and !site.css_minifier['fileSuffix'].nil? and site.css_minifier['fileSuffix'].to_s.length>0
+          fileSuffix = site.css_minifier['fileSuffix'].to_s
+        end
+
+        output = ''
 
         # Test if it's a CSS file.
         ext = File.extname(page.output_path)
         if !ext.empty?
+
           ext_txt = ext[1..-1]
-          if ext_txt == "css"
-            print "minifying css #{page.output_path} \n"
-            input = CSSminify.compress(input)
+
+          # Filtering out non-css files and those which were already minimized with added suffix.
+          if ext_txt == "css" and ( fileSuffix.nil? or !File.fnmatch("*"+fileSuffix+".css",page.output_path) )
+            print "Minifying css #{page.output_path} \n"
+            output = CSSminify.compress(input)
+          else
+            return input
           end
+
         end
 
-        input
+        # Depending whether file suffix was given we create a new file or minimize in place.
+        if fileSuffix.nil?
+
+          return output
+
+        else
+
+          oldFileName = File.basename(page.output_path).to_s
+
+          # Create new file name with suffix added
+          newFileName = oldFileName.slice(0..oldFileName.length-4)+fileSuffix+".css"
+          newOutputPath = File.join(File.dirname(page.output_path.to_s),newFileName)
+
+          # Create a temporary file with the merged content.
+          tmpOutputPath = File.join( "./_tmp/" , newFileName)
+          tmpOutputFile = File.new(tmpOutputPath,"w")
+          tmpOutputFile.write(output)
+          tmpOutputFile.close
+
+          # Add the temporary file to the list of pages for rendering phase.
+          newPage = site.engine.load_page(tmpOutputPath)
+          newPage.source_path = tmpOutputPath
+          newPage.output_path = newOutputPath
+          site.pages << newPage
+
+          # We return the input because we leave the original file untouched
+          input
+        end
       end
     end
   end
--- _ext/js_minifier.rb
+++ _ext/js_minifier.rb
@@ -22,10 +22,12 @@ require 'uglifier'
 #
 # 2. In your site.yml add:
 #
-#    js_minifier: enabled
+#    js_minifier:
+#      enabled: true
+#      fileSuffix: min
 #
-#    This setting is optional and defaults to 'enabled', it's useful when using different configurations
-#    for different runtime profiles.
+#    This setting is optional. 'enabled' defaults to 'true' and 'fileSuffix' defaults to an empty string.
+#    Which implies minification of file content in place. If 'fileSuffix' is given then a new file is created.
 #
 ##
 module Awestruct
@@ -36,21 +38,62 @@ module Awestruct
 
         # Checking if 'js_minifier' setting is provided and whether it's enabled.
         # By default, if it's not provided, we imply it's enabled.
-        if !site.js_minifier.nil? && !site.js_minifier.to_s.eql?("enabled")
+        if !site.js_minifier.nil? and !site.js_minifier['enabled'].nil? and site.js_minifier['enabled'].to_s.eql?("false")
           return input
         end
+
+        fileSuffix = nil
+        if !site.js_minifier.nil? and !site.js_minifier['fileSuffix'].nil? and site.js_minifier['fileSuffix'].to_s.length>0
+          fileSuffix = site.js_minifier['fileSuffix'].to_s
+        end
+
+        output = ''
 
         # Test if it's a javascript file.
         ext = File.extname(page.output_path)
         if !ext.empty?
           ext_txt = ext[1..-1]
-          if ext_txt == "js"
-            print "minifying javascript #{page.output_path} \n"
-            input = Uglifier.new.compile(input)
+
+          # Filtering out non-css files and those which were already minimized with added suffix.
+          if ext_txt == "js" and ( fileSuffix.nil? or !File.fnmatch("*"+fileSuffix+".js",page.output_path) )
+            print "Minifying javascript #{page.output_path} \n"
+            output = Uglifier.new.compile(input)
+            #print "+++++ CONTENT FOR "+File.basename(page.output_path).to_s+" +++++++"
+            #print inp
+          else
+            return input
           end
+
         end
 
-        input
+        # Depending whether file suffix was given we create a new file or minimize in place.
+        if fileSuffix.nil?
+
+          return output
+
+        else
+
+          oldFileName = File.basename(page.output_path).to_s
+
+          # Create new file name with suffix added
+          newFileName = oldFileName.slice(0..oldFileName.length-3)+fileSuffix+".js"
+          newOutputPath = File.join(File.dirname(page.output_path.to_s),newFileName)
+
+          # Create a temporary file with the merged content.
+          tmpOutputPath = File.join( "./_tmp/" , newFileName)
+          tmpOutputFile = File.new(tmpOutputPath,"w")
+          tmpOutputFile.write(output)
+          tmpOutputFile.close
+
+          # Add the temporary file to the list of pages for rendering phase.
+          newPage = site.engine.load_page(tmpOutputPath)
+          newPage.source_path = tmpOutputPath
+          newPage.output_path = newOutputPath
+          site.pages << newPage
+
+          # We return the input because we leave the original file untouched
+          input
+        end
       end
     end
   end
